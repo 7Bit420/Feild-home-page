b257ee8f54217ba2db4d5d4d3e1ebc2db9cc9d8c
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-11-13 16:53:39 +1100

    Update

--- templates/themes/Comuinity/static/scripts/js/navbar.js
+++ templates/themes/Comuinity/static/scripts/js/navbar.js
@@ -4,8 +4,8 @@
     }
 
     var navbar = document.querySelector('nav')
-    document.body.addEventListener('scroll', () => {
-        if (document.body.scrollTop >= 200) {
+    document.body.addEventListener('scroll', (e) => {
+        if (document.body.scrollTop >= 1) {
             navbar.style.opacity = "1"
         } else {
             navbar.style.opacity = "0"
